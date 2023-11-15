c9c4d6f1bdbccb8f96e93044e9b9f427b0f3a888
jakepitman <jakeseanp@gmail.com>
2023-11-15 15:48:40 +1300

    Move camera on positionSet change

--- app/Coalescent.tsx
+++ app/Coalescent.tsx
@@ -8,10 +8,13 @@ import { whaleAndDiversPositions } from "./positions/whaleAndDiversPositions";
 import { scatteredPositions } from "./positions/scatteredPositions";
 
 const positionSetMap = {
-  jake: jakePositions,
-  whale: whaleAndDiversPositions,
-  vertical,
-  horizontal,
+  jake: { pixelPositions: jakePositions, cameraPosition: [-13, -1, 8] },
+  whale: {
+    pixelPositions: whaleAndDiversPositions,
+    cameraPosition: [-7, 11, -8],
+  },
+  vertical: { pixelPositions: vertical, cameraPosition: [0, 0, 0] },
+  horizontal: { pixelPositions: horizontal, cameraPosition: [0, 0, 0] },
 };
 
 type PositionSetName = keyof typeof positionSetMap;
@@ -57,7 +60,7 @@ export const Coalescent = () => {
   // --
 
   const positionsAsArray = Object.keys(positionSetMap).map(
-    (key) => positionSetMap[key as keyof typeof positionSetMap]
+    (key) => positionSetMap[key as keyof typeof positionSetMap].pixelPositions
   );
   const highestNumberOfPositions = positionsAsArray.reduce(
     (accumulator, currentValue) =>
@@ -66,7 +69,7 @@ export const Coalescent = () => {
   );
 
   const [positionSet, setPositionSet] =
-    useState<keyof typeof positionSetMap>("jake");
+    useState<keyof typeof positionSetMap>("whale");
 
   // This needs to be the same as the amount of positions
   // Each position set must have exactly this many positions
@@ -86,11 +89,11 @@ export const Coalescent = () => {
     const r = (1 - a) * x + a * y;
     return Math.abs(x - y) < 0.001 ? y : r;
   }
-  useFrame((_state, delta) => {
-    //positionSetMap[positionSet].forEach((coordinateSet, i) => {
+  useFrame((state, delta) => {
+    // Update pixel positions
     [...new Array(highestNumberOfPositions)].forEach((_, i) => {
       const dummy = dummies[i];
-      const coordinateSet = positionSetMap[positionSet][i];
+      const coordinateSet = positionSetMap[positionSet].pixelPositions[i];
       if (coordinateSet) {
         dummy.position.x = lerp(dummy.position.x, coordinateSet.x, 0.025);
         dummy.position.y = lerp(dummy.position.y, coordinateSet.y, 0.025);
@@ -102,9 +105,9 @@ export const Coalescent = () => {
         const indexWithinScattered =
           i - scatteredPositions.length * numberOfTimesLengthFitsIni;
         const { x, y, z } = scatteredPositions[indexWithinScattered];
-        dummy.position.x = lerp(dummy.position.x, x, 0.025);
-        dummy.position.y = lerp(dummy.position.y, y, 0.025);
-        dummy.position.z = lerp(dummy.position.z, z, 0.025);
+        dummy.position.x = lerp(dummy.position.x, x, 0.005);
+        dummy.position.y = lerp(dummy.position.y, y, 0.005);
+        dummy.position.z = lerp(dummy.position.z, z, 0.005);
       }
       dummy.rotation.x += delta * 0.6;
       dummy.rotation.y += delta * 0.5;
@@ -114,6 +117,23 @@ export const Coalescent = () => {
       mesh.setMatrixAt(i, dummy.matrix);
     });
     mesh.instanceMatrix.needsUpdate = true;
+
+    // Update camera position
+    state.camera.position.x = lerp(
+      state.camera.position.x,
+      positionSetMap[positionSet].cameraPosition[0],
+      0.01
+    );
+    state.camera.position.y = lerp(
+      state.camera.position.y,
+      positionSetMap[positionSet].cameraPosition[1],
+      0.01
+    );
+    state.camera.position.z = lerp(
+      state.camera.position.z,
+      positionSetMap[positionSet].cameraPosition[2],
+      0.01
+    );
   });
 
   return (
