fd98c4b884cb591458dfde650728a5d481eeccb4
Jake Pitman <jakeseanp@gmail.com>
2024-01-07 23:46:41 +1300

    Clean up

--- app/_components/Experience/Spaceship.tsx
+++ app/_components/Experience/Spaceship.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import { useThree, useFrame } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
 import {
   spaceshipMobileScalingFactor,
   mobileBreakPoint,
