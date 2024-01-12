aa4ddc6a214c2dabfc79c6d95b9da532d9f6d47d
Jake Pitman <jakeseanp@gmail.com>
2024-01-13 08:10:00 +1300

    Fix precision in Coalescent

--- app/_utilities/functions/damp.tsx
+++ app/_utilities/functions/damp.tsx
@@ -1,5 +1,21 @@
-import { Euler } from "three";
-import { dampE as maathDampE } from "maath/easing";
+import { Euler, Vector3 } from "three";
+import { damp3 as maathDamp3, dampE as maathDampE } from "maath/easing";
+
+export const damp3 = (
+  current: Vector3,
+  target: Vector3 | [x: number, y: number, z: number],
+  smoothTime: number,
+  delta: number
+) =>
+  maathDamp3(
+    current,
+    target,
+    smoothTime,
+    delta,
+    Infinity,
+    (t) => 1 / (1 + t + 0.48 * t * t + 0.235 * t * t * t),
+    0.0001
+  );
 
 export const dampE = (
   current: Euler,
