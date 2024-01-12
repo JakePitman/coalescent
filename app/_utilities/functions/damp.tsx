c97ef3c3f6a9e58b461cd34ca24b778b00379bd4
Jake Pitman <jakeseanp@gmail.com>
2024-01-13 08:15:12 +1300

    Variable extraction

--- app/_utilities/functions/damp.tsx
+++ app/_utilities/functions/damp.tsx
@@ -1,21 +1,20 @@
 import { Euler, Vector3 } from "three";
 import { damp3 as maathDamp3, dampE as maathDampE } from "maath/easing";
 
+// End of animation precision. Smaller number means less jitters
+const eps = 0.0001;
+// This is just the default provided by maath, needs to be inserted
+// manually in order to get to the eps parameter
+const easingFunction = (t: number) =>
+  1 / (1 + t + 0.48 * t * t + 0.235 * t * t * t);
+
 export const damp3 = (
   current: Vector3,
   target: Vector3 | [x: number, y: number, z: number],
   smoothTime: number,
   delta: number
 ) =>
-  maathDamp3(
-    current,
-    target,
-    smoothTime,
-    delta,
-    Infinity,
-    (t) => 1 / (1 + t + 0.48 * t * t + 0.235 * t * t * t),
-    0.0001
-  );
+  maathDamp3(current, target, smoothTime, delta, Infinity, easingFunction, eps);
 
 export const dampE = (
   current: Euler,
@@ -23,12 +22,4 @@ export const dampE = (
   smoothTime: number,
   delta: number
 ) =>
-  maathDampE(
-    current,
-    target,
-    smoothTime,
-    delta,
-    Infinity,
-    (t) => 1 / (1 + t + 0.48 * t * t + 0.235 * t * t * t),
-    0.0001
-  );
+  maathDampE(current, target, smoothTime, delta, Infinity, easingFunction, eps);
