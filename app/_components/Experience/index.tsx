2476ec1f753105ce879694758e0ae4f42e15cac5
Jake Pitman <jakeseanp@gmail.com>
2024-03-28 18:57:46 +1300

    Add suspense boundary and loader

--- app/_components/Experience/index.tsx
+++ app/_components/Experience/index.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
+import { Environment, Loader } from "@react-three/drei";
 import { suspend } from "suspend-react";
+import { Suspense } from "react";
+
 const sunset = import("@pmndrs/assets/hdri/sky.exr").then(
   (module) => module.default
 );
@@ -17,21 +19,26 @@ type Props = {
 };
 export const Experience = ({ style }: Props) => {
   return (
-    <Canvas camera={{ position: initialCameraPosition }} style={{ ...style }}>
-      <Environment
-        files={suspend(sunset) as string}
-        blur={1}
-        resolution={256}
-      />
-      <ambientLight />
-      <Spaceship />
-      <Coalescent />
-      {/* This forms a backdrop that is necessary for the glass material's transmission property to work.
+    <>
+      <Canvas camera={{ position: initialCameraPosition }} style={{ ...style }}>
+        <Suspense fallback={null}>
+          <Environment
+            files={suspend(sunset) as string}
+            blur={1}
+            resolution={256}
+          />
+          <ambientLight />
+          <Spaceship />
+          <Coalescent />
+          {/* This forms a backdrop that is necessary for the glass material's transmission property to work.
       `transmission` has no effect when there is nothing behind the object, and renders grey, even if a background color is set on the Canvas */}
-      <mesh position={[0, 0, -50]}>
-        <boxGeometry args={[500, 500, 1]} />
-        <meshStandardMaterial color={"black"} />
-      </mesh>
-    </Canvas>
+          <mesh position={[0, 0, -50]}>
+            <boxGeometry args={[500, 500, 1]} />
+            <meshStandardMaterial color={"black"} />
+          </mesh>
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 };
