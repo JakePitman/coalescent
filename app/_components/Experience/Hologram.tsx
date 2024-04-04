f01fce6623c59a7c4dbba1c419363e44c2d97450
Jake Pitman <jakeseanp@gmail.com>
2024-04-04 13:47:03 +1300

    Set scale with timeout

--- app/_components/Experience/Hologram.tsx
+++ app/_components/Experience/Hologram.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Group } from "three";
 import { useGLTF } from "@react-three/drei";
@@ -18,6 +18,22 @@ export const Hologram = () => {
       ref.current.rotation.set(0, ref.current.rotation.y + delta / 2, 0);
   });
 
+  // This state is used to check if the value of 'page' has changed
+  const [previousPage, setPreviousPage] = useState<string>(page || "/");
+  const [isOpening, setIsOpening] = useState<boolean>(true);
+  useEffect(() => {
+    if (page !== previousPage) {
+      console.table({ page, previousPage });
+      page && setPreviousPage(page);
+      setIsOpening(false);
+
+      const timoutId = setTimeout(() => {
+        console.log("setting is opening to true");
+        setIsOpening(true);
+      }, 2000);
+    }
+  }, [page, previousPage, isOpening]);
+
   const geometryMap = {
     "/": Home.geometry,
     "/blog": Blog.geometry,
@@ -30,8 +46,10 @@ export const Hologram = () => {
   if (!page || !pageNames.includes(page)) return null;
   const geometry = geometryMap[page];
 
+  const scale = isOpening ? 0.04 : 0;
+
   return (
-    <group ref={ref} position={[-2.05, -1.9, 0]} scale={0.04}>
+    <group ref={ref} position={[-2.05, -1.9, 0]} scale={scale}>
       <mesh geometry={geometry}>
         <HolographicMaterial scanlineSize={0.1} fresnelOpacity={0.35} />
       </mesh>
