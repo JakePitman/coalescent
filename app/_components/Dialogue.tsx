087f30c487ebd47a8943845c23c7df5facd74f15
Jake Pitman <jakeseanp@gmail.com>
2024-04-21 18:29:24 +1200

    Add dark background to stories

--- app/_components/Dialogue.tsx
+++ app/_components/Dialogue.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const Dialogue = ({ text }: Props) => {
   return (
-    <div className="bg-blue-800">
+    <div className="bg-blue-800 border-white border-2">
       <p className="text-white">{text}</p>
     </div>
   );
