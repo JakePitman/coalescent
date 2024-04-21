cd366f7410e6d40e5fe65bc6db440990daf9bbf2
Jake Pitman <jakeseanp@gmail.com>
2024-04-21 19:45:43 +1200

    Add floating animation to Dialogue triangle

--- app/_components/Dialogue.tsx
+++ app/_components/Dialogue.tsx
@@ -11,10 +11,13 @@ export const Dialogue = ({ text }: Props) => {
   return (
     <div
       className={
-        styles.gradient + " " + "bg-blue-800 border-white border-2 p-4 rounded"
+        styles.gradient +
+        " " +
+        "bg-blue-800 border-white border-2 p-4 rounded relative"
       }
     >
       <p className="text-white">{text}</p>
+      <div className={styles.triangle + " " + "absolute bottom-1 right-1"} />
     </div>
   );
 };
