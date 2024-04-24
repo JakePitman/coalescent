612d1ff192d05875c6fad0cae37f98c034c8ea75
Jake Pitman <jakeseanp@gmail.com>
2024-04-24 15:00:17 +1200

    Remove unused code

--- stories/Dialogue.stories.tsx
+++ stories/Dialogue.stories.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Dialogue } from "../app/_components/Dialogue";
 import { PageContextProvider, usePageContext } from "@contexts/pageContext";
