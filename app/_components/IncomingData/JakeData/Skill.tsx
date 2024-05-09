58b37f3bd396794ca63e983b9c9585fcbcac81a3
Jake Pitman <jakeseanp@gmail.com>
2024-05-09 22:29:01 +1200

    Stagger icon animations

--- app/_components/IncomingData/JakeData/Skill.tsx
+++ app/_components/IncomingData/JakeData/Skill.tsx
@@ -9,29 +9,35 @@ const liVariants = {
     },
   },
 };
-const iconVariants = {
-  hidden: { right: "-70px", bottom: "-70px" },
+const iconVariants = (delay: number) => ({
+  hidden: { opacity: 0, rotate: "20deg", scale: 0.8 },
   show: {
-    right: "-28px",
-    bottom: "-28px",
+    rotate: "0deg",
+    opacity: 1,
+    scale: 1,
     transition: {
-      delay: 0.4,
-      duration: 0.7,
+      delay: delay,
+      duration: 2,
     },
   },
-};
+});
 
 type SkillProps = {
   name: string;
   Icon: React.ComponentType<{ className?: string }>;
+  iconDelay: number;
 };
-export const Skill = ({ name, Icon }: SkillProps) => (
+export const Skill = ({ name, Icon, iconDelay }: SkillProps) => (
   <motion.li
     variants={liVariants}
     className="relative bg-sky-300 my-1 w-[48.8%] text-sky-950 p-2 overflow-hidden"
   >
-    {name}
-    <motion.div className="absolute" variants={iconVariants}>
+    <p>{name}</p>
+
+    <motion.div
+      className="absolute right-[-28px] bottom-[-28px]"
+      variants={iconVariants(iconDelay)}
+    >
       <Icon className=" text-7xl text-[#5eb6e6]" />
     </motion.div>
   </motion.li>
--- app/_components/IncomingData/JakeData/Skills.tsx
+++ app/_components/IncomingData/JakeData/Skills.tsx
@@ -15,14 +15,15 @@ const variants = {
     transition: { delay: 0.2 },
   },
 };
+const childrenStaggerTime = 0.07;
 const hasChildrenVariants = {
   hidden: { opacity: 0 },
   show: {
     opacity: 1,
     transition: {
       delay: 0.3,
-      duration: 0.2,
-      staggerChildren: 0.07,
+      duration: 0.25,
+      staggerChildren: childrenStaggerTime,
     },
   },
 };
@@ -55,8 +56,13 @@ export const Skills = () => (
       animate="show"
       className="flex  w-full justify-between flex-wrap"
     >
-      {SKILLS.map((skill) => (
-        <Skill key={skill.name} name={skill.name} Icon={skill.Icon} />
+      {SKILLS.map((skill, i) => (
+        <Skill
+          key={skill.name}
+          name={skill.name}
+          Icon={skill.Icon}
+          iconDelay={i * childrenStaggerTime}
+        />
       ))}
     </motion.ul>
   </motion.div>
