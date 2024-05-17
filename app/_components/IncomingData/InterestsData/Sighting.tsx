9777c8b8e89dcce7e5738b9b01db0933b3b8474d
Jake Pitman <jakeseanp@gmail.com>
2024-05-17 15:40:51 +1200

    Add hw images

--- app/_components/IncomingData/InterestsData/Sighting.tsx
+++ app/_components/IncomingData/InterestsData/Sighting.tsx
@@ -1,13 +1,24 @@
 import classnames from "classnames";
+import Image from "next/image";
 
 export type Props = { image: null; isRTL: boolean };
 
 export const Sighting = ({ isRTL }: Props) => {
   return (
     <div
-      className={classnames("w-[30%] h-auto aspect-square outline ml-[3%]", {
-        "ml-0 mr-[3%]": isRTL,
-      })}
-    ></div>
+      className={classnames(
+        "relative w-[30%] h-auto aspect-square ml-[3%] overflow-hidden",
+        {
+          "ml-0 mr-[3%]": isRTL,
+        }
+      )}
+    >
+      <Image
+        src="/images/diving/GBR.jpeg"
+        alt="diving"
+        fill
+        style={{ objectFit: "cover" }}
+      />
+    </div>
   );
 };
