50c2bc4963999009eb81da9758642de7fb0c7c4b
Jake Pitman <jakeseanp@gmail.com>
2024-05-20 13:17:17 +1200

    Add HW <LoadingSpinner/>

--- app/_components/IncomingData/InterestsData/Sighting.tsx
+++ app/_components/IncomingData/InterestsData/Sighting.tsx
@@ -1,6 +1,7 @@
 import classnames from "classnames";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { LoadingSpinner } from "@components/LoadingSpinner";
 
 const variants = {
   hidden: { opacity: 0 },
@@ -25,7 +26,12 @@ export const Sighting = ({ imageURL, isRTL }: Props) => {
         }
       )}
     >
-      <Image src={imageURL} alt="diving" fill style={{ objectFit: "cover" }} />
+      {/* <Image src={imageURL} alt="diving" fill style={{ objectFit: "cover" }} /> */}
+      <div className="h-full border-solid border-green-300 border-2 relative">
+        <div className="absolute w-4/5 h-4/5 left-0 right-0 top-0 bottom-0 m-auto">
+          <LoadingSpinner />
+        </div>
+      </div>
     </motion.div>
   );
 };
---
+++ app/_components/LoadingSpinner.tsx
@@ -0,0 +1,5 @@
+import { motion } from "framer-motion";
+
+export const LoadingSpinner = () => {
+  return <div className="border-solid border-2 w-full h-full"></div>;
+};
