3b5f2eb688f401728a929c2412d4999d9b75cd32
Jake Pitman <jakeseanp@gmail.com>
2024-05-28 14:50:19 +1000

    Switch to react-multi-carousel and set up HW example

--- app/_components/IncomingData/InterestsData/CarouselFormat.tsx
+++ app/_components/IncomingData/InterestsData/CarouselFormat.tsx
@@ -1,8 +1,27 @@
 import Image from "next/image";
 import { createPortal } from "react-dom";
-import { Carousel } from "react-responsive-carousel";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 import { useCarouselContext } from "./carouselContext";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 
 export const CarouselFormat = () => {
   const { currentImage, setCurrentImage } = useCarouselContext();
@@ -11,9 +30,40 @@ export const CarouselFormat = () => {
   return (
     <>
       {createPortal(
-        <div className="fixed top-0 right-0 bottom-0 left-0 bg-black/30 z-[inherit] ">
-          <p>Hello carousel</p>
+        <div className="fixed top-0 right-0 bottom-0 left-0 bg-black/60 z-[inherit] flex justify-center items-center">
           <button onClick={() => setCurrentImage(null)}>x</button>
+          <Carousel
+            responsive={responsive}
+            swipeable={false}
+            draggable={false}
+            showDots={true}
+            infinite={true}
+            autoPlaySpeed={1000}
+            keyBoardControl={true}
+            customTransition="all .5"
+            transitionDuration={500}
+            containerClass="carousel-container w-[80vw]  bg-red-300"
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+          >
+            <div className="relative h-[80vh] w-full border-solid border-white">
+              <Image
+                src="/images/drones/drone.jpeg"
+                fill
+                style={{ objectFit: "contain" }}
+                alt="drone"
+              />
+            </div>
+            <div className="relative h-[80vh] w-full border-solid border-white">
+              <Image
+                src="/images/drones/drone_repair.jpeg"
+                fill
+                style={{ objectFit: "contain" }}
+                alt="drone"
+              />
+            </div>
+          </Carousel>
         </div>,
         document.getElementById("portal") as HTMLElement
       )}
