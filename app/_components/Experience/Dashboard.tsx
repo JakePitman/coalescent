95445612ac3e2c37739c7b76e408811a556898d0
Jake Pitman <jakeseanp@gmail.com>
2024-06-07 13:12:50 +0900

    Add basic selected styles

--- app/_components/Experience/Dashboard.tsx
+++ app/_components/Experience/Dashboard.tsx
@@ -13,9 +13,10 @@ type NavItemProps = {
   label: string;
   position: [number, number, number];
   handleClick: () => void;
-  // isActive: boolean;
+  isActive: boolean;
 };
-const NavItem = ({ label, position, handleClick }: NavItemProps) => {
+const NavItem = ({ label, position, handleClick, isActive }: NavItemProps) => {
+  const content = isActive ? `· ${label} ·` : label;
   return (
     <Text
       position={position}
@@ -26,7 +27,7 @@ const NavItem = ({ label, position, handleClick }: NavItemProps) => {
         handleClick();
       }}
     >
-      {label}
+      {content}
     </Text>
   );
 };
@@ -46,31 +47,37 @@ export const Dashboard = () => {
         label="Home"
         handleClick={() => handleClick("/")}
         position={[-3.35, 0, 0]}
+        isActive={page === "/"}
       />
       <NavItem
         label="Jake"
         handleClick={() => handleClick("/jake")}
         position={[-3.35, -1.2, 0]}
+        isActive={page === "/jake"}
       />
       <NavItem
         label="Interests"
         handleClick={() => handleClick("/interests")}
         position={[-3.35, -2.4, 0]}
+        isActive={page === "/interests"}
       />
       <NavItem
         label="Projects"
         handleClick={() => handleClick("/projects")}
         position={[3.35, 0, 0]}
+        isActive={page === "/projects"}
       />
       <NavItem
         label="Blog"
         handleClick={() => handleClick("/blog")}
         position={[3.35, -1.2, 0]}
+        isActive={page === "/blog"}
       />
       <NavItem
         label="Contact"
         handleClick={() => handleClick("/contact")}
         position={[3.35, -2.4, 0]}
+        isActive={page === "/contact"}
       />
     </group>
   );
