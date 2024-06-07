a38be9bc59b133e4f65016e53518f5b81e015b05
Jake Pitman <jakeseanp@gmail.com>
2024-06-07 13:58:20 +0900

    Extract x and y offsets

--- app/_components/Experience/Dashboard.tsx
+++ app/_components/Experience/Dashboard.tsx
@@ -41,6 +41,8 @@ const NavItem = ({ label, position, handleClick, isActive }: NavItemProps) => {
   );
 };
 
+const xOffset = 3.35;
+const yOffset = 1.2;
 export const Dashboard = () => {
   const router = useRouter();
   const { page, setPage } = usePageContext();
@@ -55,37 +57,37 @@ export const Dashboard = () => {
       <NavItem
         label="Home"
         handleClick={() => handleClick("/")}
-        position={[-3.35, 0, 0]}
+        position={[-xOffset, 0, 0]}
         isActive={page === "/"}
       />
       <NavItem
         label="Jake"
         handleClick={() => handleClick("/jake")}
-        position={[-3.35, -1.2, 0]}
+        position={[-xOffset, -yOffset, 0]}
         isActive={page === "/jake"}
       />
       <NavItem
         label="Interests"
         handleClick={() => handleClick("/interests")}
-        position={[-3.35, -2.4, 0]}
+        position={[-xOffset, -yOffset * 2, 0]}
         isActive={page === "/interests"}
       />
       <NavItem
         label="Projects"
         handleClick={() => handleClick("/projects")}
-        position={[3.35, 0, 0]}
+        position={[xOffset, 0, 0]}
         isActive={page === "/projects"}
       />
       <NavItem
         label="Blog"
         handleClick={() => handleClick("/blog")}
-        position={[3.35, -1.2, 0]}
+        position={[xOffset, -yOffset, 0]}
         isActive={page === "/blog"}
       />
       <NavItem
         label="Contact"
         handleClick={() => handleClick("/contact")}
-        position={[3.35, -2.4, 0]}
+        position={[xOffset, -yOffset * 2, 0]}
         isActive={page === "/contact"}
       />
     </group>
