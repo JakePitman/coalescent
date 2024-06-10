4ac17bf13fb6d5e5a6ff559356af02062acfcb94
Jake Pitman <jakeseanp@gmail.com>
2024-06-10 10:01:49 +0900

    Add text and animations

--- app/_components/IncomingData/ContactData.tsx
+++ app/_components/IncomingData/ContactData.tsx
@@ -1,27 +1,63 @@
-export const ContactData = () => (
-  <div>
-    <form action="https://formspree.io/f/xbjnnjaq" method="POST">
-      <label className="block pb-3">
-        <p className="pb-1">Your email:</p>
-        <input
-          type="email"
-          name="email"
-          className="border-sky-700 border-2 rounded bg-sky-950 w-full"
-        />
-      </label>
-      <label className="block pb-3">
-        <p className="pb-1">Your message:</p>
-        <textarea
-          name="message"
-          className="border-sky-700 border-2 rounded bg-sky-950 w-full h-60"
-        ></textarea>
-      </label>
-      <button
-        type="submit"
-        className="border-2 rounded border-sky-500 px-3 py-1 hover:bg-sky-500 hover:text-sky-950 transition-all"
-      >
-        Send
-      </button>
-    </form>
-  </div>
-);
+import { useState } from "react";
+import { TextBlock } from "@components/IncomingData/TextBlock";
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      // duration: 2,
+    },
+  },
+};
+
+export const ContactData = () => {
+  const [textIsFinished, setTextIsFinished] = useState(false);
+
+  return (
+    <div>
+      <TextBlock onTypingDone={() => setTextIsFinished(true)}>
+        <p>{"Hi there! I'm Jake."}</p>
+        <p>
+          {
+            "I heard you were looking for a front-end engineer, and I'd be happy to help. Please use the form below, and let's work together."
+          }
+        </p>
+        <p className="w-full text-center my-5">- ⎔ -</p>
+      </TextBlock>
+      {textIsFinished && (
+        <motion.form
+          initial="hidden"
+          animate="show"
+          transition={{ staggerChildren: 0.1 }}
+          action="https://formspree.io/f/xbjnnjaq"
+          method="POST"
+        >
+          <motion.label variants={variants} className="block mb-3">
+            <p className="mb-1">Your email:</p>
+            <input
+              type="email"
+              name="email"
+              className="border-sky-700 border-2 rounded bg-sky-950 w-full"
+            />
+          </motion.label>
+          <motion.label variants={variants} className="block mb-3">
+            <p className="mb-1">Your message:</p>
+            <textarea
+              name="message"
+              className="border-sky-700 border-2 rounded bg-sky-950 w-full h-60"
+            ></textarea>
+          </motion.label>
+          <motion.button
+            variants={variants}
+            type="submit"
+            className="border-2 rounded border-sky-500 px-3 py-1 hover:bg-sky-500 hover:text-sky-950 transition-all"
+          >
+            Send
+          </motion.button>
+        </motion.form>
+      )}
+    </div>
+  );
+};
