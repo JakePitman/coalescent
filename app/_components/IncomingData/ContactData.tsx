742267bf8180b9e3767d99dbe7210c95689d221f
Jake Pitman <jakeseanp@gmail.com>
2024-06-10 09:11:07 +0900

    Add hw contact form

--- app/_components/IncomingData/ContactData.tsx
+++ app/_components/IncomingData/ContactData.tsx
@@ -1,1 +1,15 @@
-export const ContactData = () => <div>{"Contact coming soon..."}</div>;
+export const ContactData = () => (
+  <div>
+    <form action="https://formspree.io/f/xbjnnjaq" method="POST">
+      <label>
+        Your email:
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Your message:
+        <textarea name="message"></textarea>
+      </label>
+      <button type="submit">Send</button>
+    </form>
+  </div>
+);
