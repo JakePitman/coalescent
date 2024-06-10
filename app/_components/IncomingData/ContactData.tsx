9ab4dbcaf3d4c6eab10f6c3814bb2a51861a9114
Jake Pitman <jakeseanp@gmail.com>
2024-06-10 09:30:09 +0900

    Add some basic styles

--- app/_components/IncomingData/ContactData.tsx
+++ app/_components/IncomingData/ContactData.tsx
@@ -1,15 +1,27 @@
 export const ContactData = () => (
   <div>
     <form action="https://formspree.io/f/xbjnnjaq" method="POST">
-      <label>
-        Your email:
-        <input type="email" name="email" />
+      <label className="block pb-3">
+        <p className="pb-1">Your email:</p>
+        <input
+          type="email"
+          name="email"
+          className="border-sky-700 border-2 rounded bg-sky-950 w-full"
+        />
       </label>
-      <label>
-        Your message:
-        <textarea name="message"></textarea>
+      <label className="block pb-3">
+        <p className="pb-1">Your message:</p>
+        <textarea
+          name="message"
+          className="border-sky-700 border-2 rounded bg-sky-950 w-full h-60"
+        ></textarea>
       </label>
-      <button type="submit">Send</button>
+      <button
+        type="submit"
+        className="border-2 rounded border-sky-500 px-3 py-1 hover:bg-sky-500 hover:text-sky-950 transition-all"
+      >
+        Send
+      </button>
     </form>
   </div>
 );
