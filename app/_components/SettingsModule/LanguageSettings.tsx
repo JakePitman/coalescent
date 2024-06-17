0d50c36e5163d19fca23755fb1981af0e06d3728
Jake Pitman <jakeseanp@gmail.com>
2024-06-17 12:38:29 +0900

    Add hw langauge options

--- app/_components/SettingsModule/LanguageSettings.tsx
+++ app/_components/SettingsModule/LanguageSettings.tsx
@@ -1,3 +1,22 @@
+type LanguageOptionProps = {
+  id: string;
+  name: string;
+  value: string;
+};
+const LanguageOption = ({ id, value }: LanguageOptionProps) => {
+  return (
+    <div>
+      <input type="radio" id={id} name="selected-language" value={value} />
+      <label htmlFor={id}>{value}</label>
+    </div>
+  );
+};
+
 export const LanguageSettings = () => {
-  return <div className="white">Language Settings</div>;
+  return (
+    <div className="flex flex-col">
+      <LanguageOption id="english" name="language" value="english" />
+      <LanguageOption id="japanese" name="language" value="japanese" />
+    </div>
+  );
 };
